e50f7ca119c8bc8f3c137edca0d8ccfeacd5f058
yeonghyeonSeok <syh4834@naver.com>
2020-10-09 04:11:31 +0900

    initial

---
+++ module/utils/responseMessage.js
@@ -0,0 +1,10 @@
+module.exports = {
+    DB_ERROR: "DB 오류",
+    NULL_VALUE: "필요한 값이 없습니다",
+    OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+
+    SIGNUP_FAIL: "회원가입 실패",
+    SIGNUP_SUCCESS: "회원가입 성공",
+    ALREADY_EXIST_ID: "이미 존재하는 아이디입니다",
+    USABLE_ID: "사용가능한 아이디입니다"
+};
